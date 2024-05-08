39717cc14915f0cbab7b62ad4e833f3a9e3e99ee
codingcyclist <mail@codingcyclist.com>
2024-05-08 18:25:03 +0200

    Finishing touches

--- alerts/__init__.py
+++ alerts/__init__.py
@@ -1,7 +1,3 @@
 # Please use this module to implement your alert automation logic.
-# Your script should be executable from the command line
-
-# - replace emails with sandbox emails for zerobounce
-# - invite myself to an alert channel
-# - add a bot to the alerts channel
-# - share the bot token with the candidate
+# Your script should be executable from the command line and you
+# should abide by general coding best practices
